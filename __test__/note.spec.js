90fde22ce0ca6f153c0c2d764ded30eca781024f
Paolo <fedeli.paolol@gmail.com.com>
2020-06-08 22:53:54 +0200

    add other Just Test

--- __test__/note.spec.js
+++ __test__/note.spec.js
@@ -2,6 +2,9 @@ const supertest = require("supertest");
 
 const app = require("../app");
 const request = supertest(app);
+const User = require('../models/UserSchema')
+const Note = require("../models/NoteSchema");
+
 
 
 describe("Test the root path", () => {
@@ -21,12 +24,52 @@ describe("Test the root path", () => {
         done();
     })
 
-    test('Sould create a new user', async () => {
+    it('Sould create a new user', async () => {
         await request
           .post("/api/user/newuser")
           .send({
             name: "Gino",
           })
-          .expect(200);
+            .expect(200);
+        const user = await User.findOne({ name: "Gino" })
+          expect(user.name).toBeTruthy();
+
     })
+
+ it("Sould create a new note", async () => {
+   await request
+     .post("/api/user/newnote")
+     .send({
+         title: "TEST 1",
+         content: 'Lorem Ipsum'
+     })
+     .expect(200)
+   const note = await Note.findOne({ title: "TEST 1" });
+     expect(note.title).toBeTruthy();
+     expect(note.content).toBeTruthy();
+ });
+    
+     it("retrive specific note", async (done) => {
+    const response = await request.post("/api/user/newnote").send({
+      title: "TEST 2",
+      content: "Cave Canem",
+    })
+         expect(200)
+         const id = response.body._id
+         console.log(id)
+         
+         const  result  = await request.get(`/api/user/${id}`);
+         const noteId = result.body._id;
+         expect(id).toBe(noteId);
+
+
+    done();
+  });
+
+
+afterEach(async () => {
+    await User.deleteMany();
+    await Note.deleteMany();
+});
+
 })
